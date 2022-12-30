0c1c01c3f2659359edae90bd373fb2f93159e260
Wahid Shuvo <wshuvo360@gmail.com>
2022-12-30 13:41:25 +0600

    commit1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");    
+      Console.WriteLine("Hello World!");  
+      task1.sss()  ;
     }
   }
 }
---
+++ task1.cs
@@ -0,0 +1,12 @@
+
+    namespace HelloWorld
+{
+    public class task1
+  {
+    static void sss()
+    {
+      Console.WriteLine("qwerty World!");    
+    }
+  }
+}
+ 
